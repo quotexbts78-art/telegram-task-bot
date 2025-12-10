8837598797761d0e457051524b6b2f01b5018156
quotexbts78-art <quotexbts78@gmail.com>
2025-12-10 22:50:10 +0530

    Add keep_alive.py to run Flask app in a thread

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+# keep_alive.py
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is Running"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
